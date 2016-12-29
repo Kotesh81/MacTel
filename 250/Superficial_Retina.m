95d9065bdf510e7f4e0241911c433874a37b4e6e
Yachee123 <yachee13@gmail.com>
2016-12-29 15:51:07 +0530

    Add files via upload

---
+++ 250/Superficial_Retina.m
@@ -0,0 +1,17 @@
+dataset = xlsread('Report_250.xlsx',1, 'A1:A18');
+y_axis = dataset(:,1);
+x_axis = (1 :18);
+p1 = plot (x_axis, y_axis,'--o');
+hold on
+xlabel('Patient No')
+ylabel('Area of Superficial Retina')
+title('Superficial Retina');
+s = sum(y_axis);
+% fprintf ('%f\n',s)
+mean = s/18; 
+fprintf ('Mean of all areas  = %f\n',mean)
+p2 = plot ([0,18],[mean,mean]);
+hold off
+legend([p1 p2],'Exact Areas','Mean')
+
+
---
+++ 250/start_process_250.m
@@ -0,0 +1,58 @@
+function start_process_250(files,folder)
+files = cellstr(files);
+for i = 1:length(files)
+fname =fullfile(folder,files{i});    
+im_ab = imread(fname);
+    
+if size(im_ab,1) ~= 250 && size(im_ab,2) ~= 250
+    im_ab = imresize((im_ab), [250 250]);
+end
+
+im_ab = rgb2gray(im_ab);
+
+binary = im2bw(im_ab);
+
+area(i) = bwarea(binary);
+fprintf('%f\n', area(i));
+final_area = num2str(area(i));
+fprintf ('%s\n', final_area);
+
+fprintf('\n');
+
+end
+
+
+z= xlsread('Report_250.xlsx');
+[m,n] = size(z);
+j = m + 1;
+k = num2str(m+1);
+
+c = strcat ('A', k);
+d = strcat ('B', k);
+e = strcat ('C', k);
+f = strcat ('D', k);
+
+   
+xlswrite('Report_250.xlsx',area(1),1,c);
+xlswrite('Report_250.xlsx',area(2),1,d);
+xlswrite('Report_250.xlsx',area(3),1,e);
+xlswrite('Report_250.xlsx',area(4),1,f);
+clear m,n,j,k;
+close;
+ 
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
