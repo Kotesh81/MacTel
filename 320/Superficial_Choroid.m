834a24e736d2f4708dbe4d6ec8eac91eb2f6a23e
Yachee123 <yachee13@gmail.com>
2016-12-31 14:55:53 +0530

    Yachee

--- 320/Superficial_Choroid.m
+++ 320/Superficial_Choroid.m
@@ -1,15 +1,15 @@
-dataset = xlsread('Report_320.xlsx',1, 'C1:C18');
-y_axis = dataset(:,1);
-x_axis = (1 :18);
-p1 = plot (x_axis, y_axis,'--o');
+dataset = xlsread('Report_320.xlsx',1,'C1:C18');           % Reading data from the Excel file.
+y_axis = dataset(:,1);                                      % Taking area as the y-axis
+x_axis = (1 :18);                                           % Taking patient no as the x-axis
+p1 = plot (x_axis, y_axis,'--o');                           % plot the graph between x-axis and y-axis
 hold on
-xlabel('Patient No')
-ylabel('Area of Superficial Choroid')
-title('Superficial Choroid');
+xlabel('Patient No')                                          % Labelling x-axis as Patient No
+ylabel('Area of Superficial Choroid')                         % Labelling y-axis as area
+title('Superficial Choroid');                               % Title of the plot is  Superficial Choroid
 s = sum(y_axis);
 % fprintf ('%f\n',s)
-mean = s/18; 
-fprintf ('Mean of all areas  =  %f\n',mean)
-p2 = plot ([0,18],[mean,mean]);
+mean = s/18;                                                    % Calculating Mean 
+fprintf ('Mean of all areas  =  %f\n',mean)             
+p2 = plot ([0,18],[mean,mean]);                     % Ploting the mean line in the plot
 hold off
-legend([p1 p2],'Exact Areas','Mean')
+legend([p1 p2],'Exact Areas','Mean')                % Labels the different lines 
